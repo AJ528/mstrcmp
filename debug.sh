927a78224d857758bd0297c61c06d0b163cb1d2c
ratman528 <adevries528@gmail.com>
2025-02-22 18:47:32 -0600

    updated debug.sh

--- debug.sh
+++ debug.sh
@@ -9,9 +9,9 @@ echo "START debug.sh"
 # also disable confirmations so you can quit by just using "q"
 gdb-multiarch -ex "set confirm off" \
     -ex "target extended-remote /dev/ttyBmpGdb" -ex "monitor swd_scan" -ex "attach 1" \
-    -ex "load cm0plus/bin/mmemcpy_cm0plus.elf" \
-    -ex "file cm4/bin/mmemcpy_cm4.elf" -ex "load" \
-    -ex "add-symbol-file cm0plus/bin/mmemcpy_cm0plus.elf"
+    -ex "load cm0plus/bin/mstrcmp_cm0plus.elf" \
+    -ex "file cm4/bin/mstrcmp_cm4.elf" -ex "load" \
+    -ex "add-symbol-file cm0plus/bin/mstrcmp_cm0plus.elf"
 
 # gdb-multiarch -ex "set confirm off" \
 #     -ex "target extended-remote /dev/ttyACM0" -ex "monitor swd_scan" -ex "attach 2" \
